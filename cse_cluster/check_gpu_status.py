a2f0a607ea654e86d4bb031561ac9e3b8aec43fd
sxjscience <xshiab@ust.hk>
2017-04-28 23:40:10 +0800

    wait for files

--- cse_cluster/check_gpu_status.py
+++ cse_cluster/check_gpu_status.py
@@ -1,6 +1,7 @@
 from subprocess import check_output
 import os
 import re
+import time
 
 gpu_clients = ["client108", "client109", "client110", "client111", "client112",
                "client113", "client114"]
@@ -26,13 +27,25 @@ sleep 10s""".format(CURRENT_DIR, CURRENT_DIR, CURRENT_DIR, client_name)
 info_str = ""
 regex = "Your job (\d+)"
 pattern = re.compile(regex)
+job_ids = []
 for name in gpu_clients:
-    info_str += "# GPU Info {}".format(name)
     print("Submitting the job for", get_gpu_info_bash(name))
     f = open("gpu_status_temp.sh", "w")
     f.write(get_gpu_info_bash(name))
     f.close()
     out = check_output(['qsub', '-q', 'all.q', 'gpu_status_temp.sh'])
     out = str(out)
-    print(pattern.findall(out)[0])
+    job_ids.append(pattern.findall(out)[0])
 os.remove("gpu_status_temp.sh")
+for name, id in zip(gpu_clients, job_ids):
+    output_filename = "gpu_info.o{}".format(id)
+    error_filename = "gpu_info.e{}".format(id)
+    while not os.path.exists(output_filename):
+        time.sleep(1)
+    info_str += "# GPU Info {}".format(name)
+    f = open(output_filename, 'r')
+    info_str += f.read()
+    f.close()
+    os.remove(output_filename)
+    os.remove(error_filename)
+print(info_str)
