5e7136bbfafe5e2731ad7e28324a8f123d06cb84
sxjscience <xshiab@ust.hk>
2017-04-28 23:04:59 +0800

    update

---
+++ cse_cluster/check_gpu_status.py
@@ -0,0 +1,31 @@
+from subprocess import check_output
+import os
+
+gpu_clients = ["client108", "client109", "client110", "client111", "client112",
+               "client113", "client114"]
+
+def get_gpu_info_bash(client_name):
+    str =\
+r"""#!/bin/csh
+# specify where the output file should be put
+#$ -o ./
+#$ -e ./
+
+# specify the working path
+#$ -wd ./
+
+#$ -l h={}
+#$ -N gpu_info
+
+setenv PATH "/usr/local/cuda-8.0/bin:$PATH"
+nvidia-smi""".format(client_name)
+    return str
+
+for name in gpu_clients:
+    print(get_gpu_info_bash(name))
+    f = open("gpu_status_temp.sh", "w")
+    f.write(get_gpu_info_bash(name))
+    out = check_output(['qsub', '-q', 'all.q', 'gpu_status_temp.sh'])
+    print(out)
+    f.close()
+os.remove("gpu_status_temp.sh")
